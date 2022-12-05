24fb23243aafedc68f82aa4faafb8b1a3979e137
Iordache Catalin <iordache.catalin1986@gmail.com>
2022-12-05 13:37:03 +0200

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-
-import './App.css';
-import FirstPage from "../src/Pages/firstPage/firstPage"
-import ContactPage from "../src/Pages/contact/contact"
-
-
+import "./App.css";
+import FirstPage from "../src/Pages/firstPage/firstPage";
+import ContactPage from "../src/Pages/contact/contact";
 
 function App() {
   return (
-
-  
-  <Routes>
-  <Route path="/curriculumVitae" element={<FirstPage />} />
-  <Route path="/" element={<FirstPage />} />
-  <Route path="/Home" element={<FirstPage />} />
-  <Route path="/contact" element={<ContactPage />} />
-  <Route path="*" element={<FirstPage />} />
-
- </Routes>
-
-  
+    <Routes>
+      <Route path="/curriculumVitae" element={<FirstPage />} />
+      <Route path="/" element={<FirstPage />} />
+      <Route path="/Home" element={<FirstPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="*" element={<FirstPage />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,23 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import FirstPage from "../src/Pages/firstPage/firstPage"
-import ContactPage from "../src/Pages/contact/contact"
+import { HashRouter  } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-  <Route path="/curriculumVitae" element={<FirstPage />} />
-  <Route path="/" element={<FirstPage />} />
-  <Route path="/Home" element={<FirstPage />} />
-  <Route path="/contact" element={<ContactPage />} />
-  <Route path="*" element={<FirstPage />} />
-
- </Routes>
-    </BrowserRouter>
+    <HashRouter >
+      <App />
+    </HashRouter >
   </React.StrictMode>
 );
