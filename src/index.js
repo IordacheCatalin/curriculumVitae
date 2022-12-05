d67c27b5ace7841205c5d7489fb01e3eae9acd46
Iordache Catalin <iordache.catalin1986@gmail.com>
2022-12-05 13:23:01 +0200

    Test routers

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import FirstPage from "../src/Pages/firstPage/firstPage"
+import ContactPage from "../src/Pages/contact/contact"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Routes>
+  <Route path="/curriculumVitae" element={<FirstPage />} />
+  <Route path="/" element={<FirstPage />} />
+  <Route path="/Home" element={<FirstPage />} />
+  <Route path="/contact" element={<ContactPage />} />
+  <Route path="*" element={<FirstPage />} />
+
+ </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
